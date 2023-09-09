0e476b355a7c6232d9c77af569ff410c169156d4
anji0114 <anji.tnk.140114@gmail.com>
2023-09-09 19:20:46 +0900

    eslintのオプション追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
-export default function Home() {
-  return <div></div>;
-}
+import { NextPage } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Image src='/' alt=''></Image>
+      <Link href=''></Link>
+    </div>
+  );
+};
+
+export default Home;
