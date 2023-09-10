58c5bd0e6fc953c8ff6cf66f6a6ca08f6387d814
anji0114 <anji.tnk.140114@gmail.com>
2023-09-10 10:04:50 +0900

    buttonコンポーネントのwrapの修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,29 @@
-'use client';
-import { Button } from '@/components/elements/Button';
-import { TrashIcon } from '@heroicons/react/24/outline';
-import { type NextPage } from 'next';
-import Image from 'next/image';
-import Link from 'next/link';
+"use client";
+import { Button } from "@/components/elements/Button";
+import { TrashIcon } from "@heroicons/react/24/outline";
+import { type NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
     <div className="space-y-4 max-w-[800px] mx-auto py-10">
       <div className="space-x-4">
-        <Link href="/">
-          <Button color="default" size="md">
-            ボタン
-          </Button>
-        </Link>
+        <Button href="/" target="_blank" color="default" size="md">
+          ボタン
+        </Button>
 
-        <button
+        <Button
+          disabled
+          color="blue"
+          size="md"
           onClick={() => {
-            alert('ok');
+            alert("ok");
           }}
+          icon={<Image src="/images/icon/github.svg" alt="github" width={20} height={20} />}
         >
-          <Button
-            color="blue"
-            size="md"
-            icon={<Image src="/images/icon/github.svg" alt="github" width={20} height={20} />}
-          >
-            ボタン
-          </Button>
-        </button>
+          ボタン
+        </Button>
 
         <Button color="default" size="sm" isLoading icon={<TrashIcon />}>
           ログイン
