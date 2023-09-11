7fbd0a59d63fbe89f9096cad67a9765e983e05f2
anji0114 <anji.tnk.140114@gmail.com>
2023-09-11 22:01:12 +0900

    prismaのセットアップ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
 import { NextPage } from 'next';
-import Image from 'next/image';
-import Link from 'next/link';
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Image src='/' alt=''></Image>
-      <Link href=''></Link>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
