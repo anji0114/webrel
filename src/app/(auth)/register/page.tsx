2e62dea970ef7a83294d2173fac746089a0ded1d
anji0114 <anji.tnk.140114@gmail.com>
2023-09-25 06:26:17 +0900

    Metaデータの設定

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,5 +1,10 @@
-'use client';
+import { Metadata } from 'next';
 import { AuthContent } from '@/features/auth/components/AuthContent';
+
+export const metadata: Metadata = {
+  title: '新規会員登録 | webrel',
+  description: 'URLベースでタスク・情報を管理する',
+};
 
 const page = () => {
   return <AuthContent title='Webrelにログインする' type='register' />;
