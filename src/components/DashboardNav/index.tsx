01030a282abe89af29ce69dd444b8a03f5a45f0d
anji0114 <anji.tnk.140114@gmail.com>
2023-10-03 21:55:03 +0900

    メニュー部分のデザイン修正

--- src/components/DashboardNav/index.tsx
+++ src/components/DashboardNav/index.tsx
@@ -1,7 +1,12 @@
 'use client';
 
-import { ArrowLeftOnRectangleIcon } from '@heroicons/react/24/outline';
+import {
+  ArrowLeftOnRectangleIcon,
+  BellAlertIcon,
+  Cog8ToothIcon,
+} from '@heroicons/react/24/outline';
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+import Link from 'next/link';
 import { User } from 'next-auth';
 import { signOut } from 'next-auth/react';
 import { FC } from 'react';
@@ -14,21 +19,39 @@ type TDashboardNavProps = {
 export const DashboardNav: FC<TDashboardNavProps> = ({ user }) => {
   return (
     <DropdownMenu.Root>
-      <DropdownMenu.Trigger>
+      <DropdownMenu.Trigger className='outline-none'>
         <Avatar src={user.image ? user.image : '/images/dummy/image1.png'} />
       </DropdownMenu.Trigger>
       <DropdownMenu.Portal>
         <DropdownMenu.Content
-          className='z-[101]  w-[180px] py-4 bg-white border border-gray-400 rounded-lg'
+          className='z-[101] w-[180px] py-2 bg-white border border-gray-300 rounded-lg shadow'
           align='end'
           sideOffset={10}
         >
           <DropdownMenu.Item className='hover:outline-none'>
+            <Link
+              href='/dashboard/notice'
+              className='text-sm px-4 py-2 w-full flex items-center gap-2 text-left hover:bg-gray-200'
+            >
+              <BellAlertIcon className='w-4' />
+              招待
+            </Link>
+          </DropdownMenu.Item>
+          <DropdownMenu.Item className='hover:outline-none'>
+            <Link
+              href='/dashboard/setting'
+              className='text-sm px-4 py-2 w-full flex items-center gap-2 text-left hover:bg-gray-200'
+            >
+              <Cog8ToothIcon className='w-4' />
+              設定
+            </Link>
+          </DropdownMenu.Item>
+          <DropdownMenu.Item className='hover:outline-none'>
             <button
-              className='px-4 py-3 w-full text-left hover:bg-gray-100'
+              className='text-sm px-4 py-2 w-full flex items-center gap-2 text-left hover:bg-gray-200'
               onClick={() => signOut()}
             >
-              <ArrowLeftOnRectangleIcon />
+              <ArrowLeftOnRectangleIcon className='w-4' />
               ログアウト
             </button>
           </DropdownMenu.Item>
