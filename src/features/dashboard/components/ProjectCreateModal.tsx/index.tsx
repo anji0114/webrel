ad752f2c731092f836a70929e3cd12d98daff99c
anji0114 <anji.tnk.140114@gmail.com>
2023-10-16 22:13:06 +0900

    型の修正とファイルの場所の修正

--- src/features/dashboard/components/ProjectCreateModal.tsx/index.tsx
+++ src/features/dashboard/components/ProjectCreateModal.tsx/index.tsx
@@ -1,14 +1,11 @@
 'use client';
 
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { useRouter } from 'next/navigation';
 import { FC } from 'react';
 import { useForm } from 'react-hook-form';
 import { Input } from '@/components/elements/Input';
 import { Modal, TModalProps } from '@/components/elements/Modal';
 import { Textarea } from '@/components/elements/Textarea';
-import { QUERY_KEYS } from '@/constants/queryKey';
-import { createProjectApi } from '@/services/projectApi';
+import { useCreateProject } from '@/services/projectApi';
 
 type TProjectCreateModalProps = Omit<
   TModalProps,
@@ -24,24 +21,9 @@ export const ProjectCreateModal: FC<TProjectCreateModalProps> = ({
   open,
   onCancel,
 }) => {
-  const router = useRouter();
-  const queryClient = useQueryClient();
   const { register, getValues, watch } = useForm<FormValues>();
   const nameValue = watch('name');
-
-  const { mutate: createProject, isLoading } = useMutation({
-    mutationFn: async (payload: { name: string; description?: string }) => {
-      const data = await createProjectApi(payload);
-      return data;
-    },
-    onSuccess: (data) => {
-      queryClient.invalidateQueries([QUERY_KEYS.PROJECT.FETCH_PROJECTS]);
-      router.push(`/project/${data.id}`);
-    },
-    onError: () => {
-      console.log('失敗');
-    },
-  });
+  const { createProject, isLoading } = useCreateProject();
 
   return (
     <>
