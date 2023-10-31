30ba1c1033cd6da26d8be0bfc57f0ad15dfdd620
anji0114 <anji.tnk.140114@gmail.com>
2023-10-31 09:27:37 +0900

    ボタンコンポーネントのテスト

--- __test__/home.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Home from '@/app/(lp)/page';
-
-it('should have docs text', () => {
-  render(<Home />);
-  const myElem = screen.getByText('Yes');
-  expect(myElem).toBeInTheDocument();
-});
---
+++ src/components/elements/Button/Button.test.tsx
@@ -0,0 +1,58 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import { Button } from '.';
+
+describe('Button render', () => {
+  test('display button', () => {
+    render(
+      <Button color='dark' size='md'>
+        Button
+      </Button>,
+    );
+    expect(screen.getByRole('button')).toBeInTheDocument();
+  });
+
+  test('renders as an anchor tag when href is provided', () => {
+    render(
+      <Button href='/sample' color='dark' size='md'>
+        Link
+      </Button>,
+    );
+    expect(screen.getByRole('link')).toBeInTheDocument();
+  });
+});
+
+describe('Button interactions', () => {
+  test('calls onClick handler when clicked', () => {
+    const handleClick = jest.fn();
+    render(
+      <Button color='dark' size='md' onClick={handleClick}>
+        Button
+      </Button>,
+    );
+    fireEvent.click(screen.getByRole('button'));
+    expect(handleClick).toHaveBeenCalled();
+  });
+
+  test('does not call onClick handler when disabled', () => {
+    const handleClick = jest.fn();
+    render(
+      <Button color='dark' size='md' onClick={handleClick} disabled>
+        Button
+      </Button>,
+    );
+    fireEvent.click(screen.getByRole('button'));
+    expect(handleClick).not.toHaveBeenCalled();
+  });
+});
+
+test('Button snapshot', () => {
+  const tree = renderer
+    .create(
+      <Button color='dark' size='md'>
+        Button
+      </Button>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
